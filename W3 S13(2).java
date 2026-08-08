58657b207300d086ee0c9fa72bc4a8f119d1db67
vtu30042-dhanraj <vtu30042@veltech.edu.in>
2026-08-08 11:44:43 +0530

    Create W3 S13(2).java

---
+++ W3 S13(2).java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int size = sc.nextInt();
+
+            ArrayList<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j < size; j++) {
+                row.add(sc.nextInt());
+            }
+
+            list.add(row);
+        }
+
+        int queries = sc.nextInt();
+
+        for (int i = 0; i < queries; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            try {
+                System.out.println(list.get(x - 1).get(y - 1));
+            } catch (Exception e) {
+                System.out.println("ERROR!");
+            }
+        }
+
+        sc.close();
+    }
+}
