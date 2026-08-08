17f54c8f43ca927c905bab1a7391cb56b522d3e9
vtu30042-dhanraj <vtu30042@veltech.edu.in>
2026-08-08 11:43:21 +0530

    Create W3 S12(2).java

---
+++ W3 S12(2).java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+        return date.getDayOfWeek().toString().charAt(0) 
+            + date.getDayOfWeek().toString().substring(1).toLowerCase();
+    }
+}
